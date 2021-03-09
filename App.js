aadeda0731a219811ce88ac7c4af4c42ed6044d2
paulmboyce <paul.boyce@goobito.com>
2021-03-09 13:12:11 +0000

    feature(app): basic goal enry and list display

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	TextInput,
+	Text,
+	View,
+	Button,
+	ScrollView,
+	FlatList,
+} from "react-native";
+
+import InputButton from "./components/InputButton";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [enteredGoal, setEnteredGoal] = useState("");
+	const [goals, setGoals] = useState([]);
+
+	const goalInputHandler = (enteredText) => {
+		setEnteredGoal(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		if (enteredGoal !== "") {
+			setGoals((current) => [
+				...current,
+				{ value: enteredGoal, id: Date.now().toString() },
+			]);
+		}
+	};
+
+	const renderItemData = (itemData) => {
+		return (
+			<View style={styles.goalDisplay} key={itemData.item.id}>
+				<Text>{itemData.item.value}</Text>
+				<Button title="x" />
+			</View>
+		);
+	};
+
+	return (
+		<View style={styles.screen}>
+			<View style={styles.enterGoal}>
+				<TextInput
+					placeholder="Enter a goal"
+					style={styles.goalInput}
+					value={enteredGoal}
+					onChangeText={goalInputHandler}
+				></TextInput>
+				<Button title="ADD" onPress={addGoalHandler} />
+			</View>
+			<FlatList data={goals} renderItem={renderItemData} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	screen: {
+		flex: 1,
+		padding: 50,
+		backgroundColor: "beige",
+	},
+
+	enterGoal: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignContent: "center",
+		marginTop: 40,
+	},
+
+	goalInput: {
+		padding: 10,
+		borderColor: "black",
+		borderWidth: 0.5,
+		flex: 1,
+	},
+
+	goalDisplay: {
+		flexDirection: "row",
+		justifyContent: "flex-end",
+		alignItems: "center",
+		paddingHorizontal: 45,
+		backgroundColor: "white",
+		borderColor: "black",
+		borderWidth: 0.5,
+		marginVertical: 3,
+	},
 });
